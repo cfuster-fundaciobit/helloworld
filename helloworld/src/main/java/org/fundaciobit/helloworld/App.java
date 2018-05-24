29e78c624184144cdeb0e94234a101cb2abaca34
cfuster-fundaciobit <cfuster@fundaciobit.org>
2018-05-24 13:46:25 +0200

    1ª versió Hello world #3

---
+++ helloworld/src/main/java/org/fundaciobit/helloworld/App.java
@@ -0,0 +1,23 @@
+package org.fundaciobit.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      
+        System.out.println( "Hello World!" );
+
+
+    }
+    
+    public int suma(int a, int b){
+    
+    return a+b;
+    
+    }
+}
+
